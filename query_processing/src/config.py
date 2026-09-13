4d0e00529f366248367d5298c44d9f4873fb55d7
zrygan <zr.gntn@gmail.com>
2026-09-13 23:50:29 +0800

    Config as the single source of truth (akin to the Bibble)

---
+++ query_processing/src/config.py
@@ -0,0 +1,21 @@
+import os
+from pathlib import Path
+
+APP_DIR = Path(__file__).resolve().parent.parent
+
+QUERIES_DIR = Path("/app/queries") if Path("/app/queries").exists() else APP_DIR / "queries"
+RESULTS_DIR = Path("/app/results") if Path("/app/results").exists() else APP_DIR / "results"
+
+DUMP_FILE = Path("dump.sql")
+
+RESULTS_DIR.mkdir(parents=True, exist_ok=True)
+
+# [fr: @zrygan; to: @all]
+# Keep it as os.environ[.] instead of os.getenv(key=.)
+# So that it crashes IMMEDIATELY if at least one environment variable
+# is not available. This acts as an ASSERT.
+DB_HOST = os.environ["DB_HOST"]
+DB_PORT = int(os.environ["DB_PORT"])
+DB_USER = os.environ["DB_USER"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
+DB_NAME = os.environ["DB_NAME"]
